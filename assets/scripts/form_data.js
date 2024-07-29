3a5d8685bf7232acdbdb22dd408346417973e8f3
Andres Castillo <andres2004_cj@outlook.com>
2024-07-29 16:13:50 -0600

    error message inputs

--- assets/scripts/form_data.js
+++ assets/scripts/form_data.js
@@ -2,13 +2,19 @@ let d = document;
 let nameInput;
 let emailInput;
 let passwordInput;
+let nameError;
+let emailError;
+let passwordError;
 
 export default function formData(formClass) {
     let form = d.querySelector(formClass);
 
     nameInput = form.querySelector("#name");
+    nameError = form.querySelector("#name-error");
     emailInput = form.querySelector("#email");
+    emailError = form.querySelector("#email-error");
     passwordInput = form.querySelector("#password");
+    passwordError = form.querySelector("#password-error");
 
     form.addEventListener("submit", (e) => {
         e.preventDefault();
@@ -24,6 +30,7 @@ export default function formData(formClass) {
         let flag = validateForm(name, email, password);
 
         if (flag) {
+            resetInputs();
             form.reset();
             alert("Formulario enviado");
         }
@@ -32,10 +39,25 @@ export default function formData(formClass) {
     d.addEventListener("click", (e) => {
         if (e.target === nameInput || e.target === emailInput || e.target === passwordInput) {
             e.target.classList.remove("error");
+
         }
+
+        if(e.target === nameInput) nameError.classList.remove("active");
+        if(e.target === emailInput) emailError.classList.remove("active");
+        if(e.target === passwordInput) passwordError.classList.remove("active");
+        
     });
 }
 
+const resetInputs = () => {
+    nameInput.classList.remove("error");
+    emailInput.classList.remove("error");
+    passwordInput.classList.remove("error");
+
+    nameError.classList.remove("active");
+    emailError.classList.remove("active");
+    passwordError.classList.remove("active");
+}
 
 const validateName = (name) => {
     let validRegex = /^[a-zA-Z]{3,16}$/;
@@ -59,16 +81,19 @@ function validateForm(name, email, password) {
 
     if (!validateName(name)) {
         nameInput.classList.add("error");
+        nameError.classList.add("active");
         flag = false;
     }
 
     if (!validateEmail(email)) {
         emailInput.classList.add("error");
+        emailError.classList.add("active");
         flag = false;
     }
 
     if (!validatePassword(password)) {
         passwordInput.classList.add("error");
+        passwordError.classList.add("active");
         flag = false;
     }
 
